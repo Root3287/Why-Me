e97d01ba84d09e8b7b85f89df126b2b8aa554cfb
Timothy Gibbons <tjgibbons10@gmail.com>
2015-07-15 21:50:36 -0500

    Innital Commit

---
+++ WhyMe-desktop/src/me/timothy/whyme/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.timothy.whyme.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.timothy.whyme.whyme;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new whyme(), config);
+	}
+}
