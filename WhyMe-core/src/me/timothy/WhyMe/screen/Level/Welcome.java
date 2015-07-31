43f2558635c903c2b935864f46cfdd9fc47086eb
Timothy Gibbons <tjgibbons@gmail.com>
2015-07-30 20:37:35 -0500

    Accidental Start...
    Forgot to make a branch of this new project

--- WhyMe-core/src/me/timothy/WhyMe/screen/Level/Welcome.java
+++ WhyMe-core/src/me/timothy/WhyMe/screen/Level/Welcome.java
@@ -1,5 +1,62 @@
 package me.timothy.WhyMe.screen.Level;
 
-public class Welcome {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class Welcome implements Screen{
+	private Stage stage;
+	private Skin skin;
+	private SpriteBatch batch;
+	
+	@Override
+	public void show() {
+		skin = new Skin(Gdx.files.internal("ui/Menu.json"), new TextureAtlas("ui/buttons.pack"));
+		stage = new Stage();
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		
+		batch.begin();
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+		
+	}
+
+	@Override
+	public void pause() {
+		
+		
+	}
+
+	@Override
+	public void resume() {
+		
+		
+	}
+
+	@Override
+	public void hide() {
+		
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+		
+	}
 
 }
